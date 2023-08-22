0417516441b43b81317f6241493b91a38f89b584
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-08-21 22:36:56 -0400

    implemented word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.date[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/hello${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
-}
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-function handleKeywordChange(event) {
-  setKeyword(event.target.value);
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
-
-return (
-  <div className="Dictionary">
-    <form onSubmit={search}>
-      <input type="search" onChange={handleKeywordChange} />
-    </form>
-  </div>
-);
