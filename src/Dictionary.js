a7628f15f26839434efe45b4ee8c2a8ded69b93c
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-08-21 21:29:28 -0400

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.date[0]);
+  }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
   }
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
-
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-    </div>
-  );
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/hello${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
 }
+
+function handleKeywordChange(event) {
+  setKeyword(event.target.value);
+}
+
+return (
+  <div className="Dictionary">
+    <form onSubmit={search}>
+      <input type="search" onChange={handleKeywordChange} />
+    </form>
+  </div>
+);
