8325facf21db26bc901af1c21545a0177702752d
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-08-28 20:31:47 -0400

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -12,7 +12,7 @@ export default function Photos(props) {
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
-                    alt="${keword}"
+                    alt="description"
                     className="img-fluid"
                   />
                 </a>
