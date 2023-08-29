7398c5eb70d6896891014b93208f7314e1750337
spoe-code <124516124+spoe-code@users.noreply.github.com>
2023-08-28 20:18:33 -0400

    added photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "0hI1N1aDsM40rxrnQfN05AugwALEKDq6LoSuJvXAS5ivwacs3SYCV77s";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
@@ -25,7 +40,7 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <section>
-        <form onSubmit={search}>
+        <form onSubmit={handleSubmit}>
           <input
             type="search"
             placeholder="Search for a word..."
@@ -37,6 +52,7 @@ export default function Dictionary() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
